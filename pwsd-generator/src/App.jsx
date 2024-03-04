82522a0019ef371b517fb002ed75b586c5c5ac61
juszkn <justasze@yahoo.com>
2024-03-04 11:27:29 +0200

    contact form task

---
+++ pwsd-generator/src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css'
+import {useState} from "react";
+
+const App = () =>  {
+    const [data, setData] = useState ({
+        name: '',
+        topic: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        // console.log(e.target.value);
+        // console.log(e.target.name);
+        setData({...data, [e.target.name] : e.target.value });
+
+    }
+
+  return (
+    <>
+        <h2> Password generator </h2>
+        <form>
+            <div className="mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    name="name"
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <div className="mb-3">
+                    <input
+                        className="form-control"
+                        placeholder="Topic"
+                        name="topic"
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+            <div>
+                <div className="mb-3">
+                    <textarea
+                        className="form-control"
+                        placeholder="Message"
+                        name="message"
+                        onChange={handleChange}
+                    ></textarea>
+                </div>
+            </div>
+            <div>
+                <div>Name: {data.name}</div>
+                <div>Topic:{data.topic}</div>
+                <div>Message: {data.message}</div>
+            </div>
+        </form>
+    </>
+  )
+}
+
+export default App
